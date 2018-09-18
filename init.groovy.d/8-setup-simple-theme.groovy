2016019415e6c22c3514d49cada52fd2bc6ad554
luktom <luktom@luktom.net>
2018-09-18 13:45:27 +0200

    Quick commit

--- init.groovy.d/8-setup-simple-theme.groovy
+++ init.groovy.d/8-setup-simple-theme.groovy
@@ -3,7 +3,22 @@ import hudson.model.*
 def themeColor = System.getenv('JENKINS_THEME_COLOR') ?: 'grey'
 
 for (pd in PageDecorator.all()) {
+
   if (pd instanceof org.codefirst.SimpleThemeDecorator) {
-    pd.cssUrl = "https://cdn.rawgit.com/afonsof/jenkins-material-theme/gh-pages/dist/material-${themeColor}.css"
+    def found = false
+
+    for(x in pd.getElements()) {
+      if(x.url.startsWith("https://cdn.rawgit.com/afonsof/jenkins-material-theme/gh-pages/dist/material")) {
+        x.url = "https://cdn.rawgit.com/afonsof/jenkins-material-theme/gh-pages/dist/material-${themeColor}.css"
+        found = true
+      }
+    }
+
+    if (!found) {
+      def te =  new org.jenkinsci.plugins.simpletheme.CssUrlThemeElement("https://cdn.rawgit.com/afonsof/jenkins-material-theme/gh-pages/dist/material-${themeColor}.css")
+
+      pd.setElements([te])
+    }
   }
 }
+
